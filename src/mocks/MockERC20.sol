383b18bee76a24a4840e74b289908cf1ea830828
kiko <229111921+kiko21213@users.noreply.github.com>
2026-04-13 18:24:37 +0300

    test: add initial deployment test

--- src/mocks/MockERC20.sol
+++ src/mocks/MockERC20.sol
@@ -3,11 +3,9 @@ pragma solidity ^0.8.29;
 
 import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
-
-contract MockERC20 is ERC20{
-
+contract MockERC20 is ERC20 {
     constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {}
-    
+
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
     }
--- test/unit/ProjectSetUp.t.sol
+++ test/unit/ProjectSetUp.t.sol
@@ -8,4 +8,25 @@ import {Router} from "src/periphery/Router.sol";
 import {MockERC20} from "src/mocks/MockERC20.sol";
 
 contract ProjectSetupTest is Test {
+    LiquidityPool internal pool;
+    PoolFactory internal factory;
+    Router internal router;
+    MockERC20 internal tokenA;
+    MockERC20 internal tokenB;
+
+    function setUp() public {
+        pool = new LiquidityPool();
+        factory = new PoolFactory();
+        router = new Router();
+        tokenA = new MockERC20("Token A", "TKA");
+        tokenB = new MockERC20("Token B", "TKB");
+    }
+
+    function test_ProjectBaseDeploys() public {
+        assertTrue(address(pool) != address(0));
+        assertTrue(address(factory) != address(0));
+        assertTrue(address(router) != address(0));
+        assertTrue(address(tokenA) != address(0));
+        assertTrue(address(tokenB) != address(0));
+    }
 }
