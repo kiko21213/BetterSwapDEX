44e5a4cc59c38dde8cf726789b25be1951d3c00c
kiko <229111921+kiko21213@users.noreply.github.com>
2026-04-21 18:00:48 +0300

    feat(PoolFactory): add createPool

--- src/core/PoolFactory.sol
+++ src/core/PoolFactory.sol
@@ -1,4 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.29;
 
-contract PoolFactory {}
+import {LiquidityPool} from "src/core/LiquidityPool.sol";
+
+contract PoolFactory {
+    address[] public allPools;
+
+    mapping(address => mapping(address => address)) public getPools;
+
+    event PoolAdded(address indexed owner, address tokenA, address tokenB, address pool);
+
+    error PoolFactory__ZeroAddress();
+    error PoolFactory__AlreadyExist();
+
+    function createPool(address tokenA, address tokenB) external {
+        if (tokenA == address(0) || tokenB == address(0)) revert PoolFactory__ZeroAddress();
+        if (getPools[tokenA][tokenB] != address(0)) revert PoolFactory__AlreadyExist();
+        LiquidityPool pool = new LiquidityPool(tokenA, tokenB);
+        address poolAddress = address(pool);
+
+        getPools[tokenA][tokenB] = poolAddress;
+        allPools.push(poolAddress);
+
+        emit PoolAdded(msg.sender, tokenA, tokenB, poolAddress);
+    }
+}
